5ca9363c7475beb210356f2cd408d19c033c05b6
victor141516 <yomellamovictor@gmail.com>
2017-02-12 17:00:08 +0100

    Amos ahi

--- server.js
+++ server.js
@@ -1,29 +1,48 @@
 var express = require('express')
-  , logger = require('morgan')
-  , jade = require('jade')
-  , app = express()
+    , logger = require('morgan')
+    , jade = require('jade')
+    , app = express()
+    , bodyParser = require('body-parser')
+    , multer  = require('multer')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res, next) {
-  try {
-    var html = jade.renderFile(__dirname + '/source/views/homepage.jade')
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+    try {
+        var html = jade.renderFile(__dirname + '/source/views/homepage.jade')
+        res.send(html)
+    } catch (e) {
+        next(e)
+    }
 })
 
 app.get('/upload', function (req, res, next) {
-  try {
-    var html = jade.renderFile(__dirname + '/source/views/upload.jade')
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+    try {
+        var html = jade.renderFile(__dirname + '/source/views/upload.jade')
+        res.send(html)
+    } catch (e) {
+        next(e)
+    }
+})
+
+app.post('/upload', function (req, res, next) {
+    try {
+        if (undefined !== req.body.magnet) {
+            res.send(req.body.magnet)
+        } else if (undefined !== req.body.torrent) {
+            console.log(req.body)
+            res.send(req.body)
+        }
+    } catch (e) {
+        next(e)
+    }
 })
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+    console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
