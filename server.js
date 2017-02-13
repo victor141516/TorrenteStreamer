442c91cc4e4434c38b7d40876f687ae2c51f12a7
victor141516 <yomellamovictor@gmail.com>
2017-02-13 03:58:20 +0100

    Ya va todonpm run watch

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var express = require('express')
   , app = express()
   , bodyParser = require('body-parser')
   , multer  = require('multer')
-  , upload = multer({ dest: 'uploads/' })
+  , storage = multer.memoryStorage()
+  , upload = multer({ storage: storage })
+  , torrentClient = new (require('webtorrent'))
+  , Transcoder = require('stream-transcoder')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -34,12 +37,45 @@ app.get('/upload', function (req, res, next) {
 app.post('/upload', upload.single('torrent'), function (req, res, next) {
     try {
         if (undefined !== req.body.magnet) {
-            res.send(req.body.magnet)
-        } else if (undefined !== req.body.torrent) {
-            console.log(req.file)
-            res.send(req.file)
+            magnet_torrent = req.body.magnet
+            name = magnet_torrent
+        } else if (undefined !== req.file) {
+            magnet_torrent = req.file.buffer
+            name = req.file.originalname
         }
+        torrentClient.add(magnet_torrent, { path: __dirname + '/downloads/' + name }, function (torrent) {
+            var maxLength = 0
+            var correctFile = null
+            for (var i = torrent.files.length - 1; i >= 0; i--) {
+                file = torrent.files[i]
+                if (file.length > maxLength) {
+                    correctFile = file
+                    maxLength = file.length
+                }
+            }
+            stream = correctFile.createReadStream()
+
+            res.writeHead(200, {
+                'Content-Type': 'video/*'
+            })
+
+            new Transcoder(stream)
+                .maxSize(320, 240)
+                .videoCodec('mpeg4')
+                .videoBitrate(800 * 1000)
+                .fps(25)
+                .audioCodec('libvorbis')
+                .sampleRate(44100)
+                .channels(2)
+                .audioBitrate(128 * 1000)
+                .format('mp4')
+                .on('finish', function() {
+                    next();
+                })
+                .stream().pipe(res)
+        })
     } catch (e) {
+        console.log(e)
         next(e)
     }
 })
