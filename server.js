46e4dd8464882c24739198d05446463335799513
victor141516 <yomellamovictor@gmail.com>
2017-02-13 15:26:17 +0100

    Ya va todo!!!

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.post('/upload', upload.single('torrent'), function (req, res, next) {
         torrentClient.add(magnet_torrent, { path: __dirname + '/downloads/' + name }, function (torrent) {
             var maxLength = 0
             var correctFile = null
+            var torrentHash = torrent.infoHash
             for (var i = torrent.files.length - 1; i >= 0; i--) {
                 file = torrent.files[i]
                 if (file.length > maxLength) {
@@ -57,13 +58,9 @@ app.post('/upload', upload.single('torrent'), function (req, res, next) {
             stream = correctFile.createReadStream()
 
             new Transcoder(stream)
-                // .maxSize(320, 240)
                 .videoCodec('libx264')
                 .videoBitrate(req.body.quality * 1000)
-                // .fps(25)
                 .audioCodec('aac')
-                // .sampleRate(44100)
-                // .channels(2)
                 .audioBitrate(128 * 1000)
                 .format('mp4')
                 .on('finish', function() {
@@ -74,11 +71,53 @@ app.post('/upload', upload.single('torrent'), function (req, res, next) {
                 })
                 .on('metadata', function(info) {
                     res.writeHead(200, {
+                        'Content-Disposition': 'inline; filename="' + torrentHash + '.mp4"',
                         'Content-Type': 'video/*',
                         'Content-Length': info.input.duration * req.body.quality / 8
                     })
                 })
                 .stream().pipe(res)
+        })
+
+        torrentClient.on('error', function (error) {
+            error_string = error.toString()
+            if (error_string.indexOf("duplicate")!=-1) {
+                var torrentHash = error_string.split(" ").pop()
+                torrent = torrentClient.get(torrentHash)
+
+                var maxLength = 0
+                var correctFile = null
+                for (var i = torrent.files.length - 1; i >= 0; i--) {
+                    file = torrent.files[i]
+                    if (file.length > maxLength) {
+                        correctFile = file
+                        maxLength = file.length
+                    }
+                }
+
+                stream = correctFile.createReadStream()
+
+                new Transcoder(stream)
+                    .videoCodec('libx264')
+                    .videoBitrate(req.body.quality * 1000)
+                    .audioCodec('aac')
+                    .audioBitrate(128 * 1000)
+                    .format('mp4')
+                    .on('finish', function() {
+                        next();
+                    })
+                    .on('error', function() {
+                        res.send("Error processing video")
+                    })
+                    .on('metadata', function(info) {
+                        res.writeHead(200, {
+                            'Content-Disposition': 'inline; filename="' + torrentHash + '.mp4"',
+                            'Content-Type': 'video/*',
+                            'Content-Length': info.input.duration * req.body.quality / 8
+                        })
+                    })
+                    .stream().pipe(res)
+            }
         })
     } catch (e) {
         console.log(e)
