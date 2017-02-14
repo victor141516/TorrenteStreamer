a78ed8f20ffa3f18d69fd33c472bc90c0a7e719b
victor141516 <yomellamovictor@gmail.com>
2017-02-14 03:00:11 +0100

    Ya va todo!!!

--- server.js
+++ server.js
@@ -16,16 +16,64 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res, next) {
+app.get('/v/q:quality/:hash_url', function (req, res, next) {
     try {
-        var html = jade.renderFile(__dirname + '/source/views/homepage.jade')
-        res.send(html)
+        hash = req.params.hash_url.split(".mp4")[0]
+        var torrent = torrentClient.get(hash)
+        var maxLength = 0
+        var correctFile = null
+        for (var i = torrent.files.length - 1; i >= 0; i--) {
+            file = torrent.files[i]
+            if (file.length > maxLength) {
+                correctFile = file
+                maxLength = file.length
+            }
+        }
+
+        stream = correctFile.createReadStream()
+
+        out = new Transcoder(stream)
+            .videoCodec('libx264')
+            .videoBitrate(req.params.quality * 1000)
+            .audioCodec('aac')
+            .audioBitrate(128 * 1000)
+            .format('mp4')
+            .on('finish', function() {
+                next();
+            })
+
+        out.on('metadata', function(info) {
+            var size = info.input.duration * req.params.quality / 8
+
+            if (!req.headers.range) {
+                res.writeHead(200, {
+                    'Content-Type': 'video/*',
+                    'Content-Range': 'bytes 0-' + (size-1).toString() + '/' + size.toString(),
+                    'Accept-Ranges': 'bytes',
+                    'Content-Length': size.toString()
+                })
+            } else {
+                var positions = req.headers.range.replace(/bytes=/, "").split("-")
+                var start = parseInt(positions[0], 10)
+                var dif = (size - start) + 1;
+
+                res.writeHead(200, {
+                    'Content-Type': 'video/*',
+                    'Content-Range': 'bytes ' + start.toString() + '-' + (size-1).toString() + '/' + size.toString(),
+                    'Accept-Ranges': 'bytes',
+                    'Content-Length': dif.toString()
+                })
+            }
+        })
+
+        out.stream().pipe(res)
     } catch (e) {
+        console.log(e)
         next(e)
     }
 })
 
-app.get('/upload', function (req, res, next) {
+app.get('/', function (req, res, next) {
     try {
         var html = jade.renderFile(__dirname + '/source/views/upload.jade')
         res.send(html)
@@ -36,97 +84,25 @@ app.get('/upload', function (req, res, next) {
 
 app.post('/upload', upload.single('torrent'), function (req, res, next) {
     try {
-        if (undefined !== req.body.magnet) {
+        if (undefined !== req.body.magnet && req.body.magnet !== '') {
             magnet_torrent = req.body.magnet
             name = magnet_torrent
         } else if (undefined !== req.file) {
             magnet_torrent = req.file.buffer
             name = req.file.originalname
+        } else {
+            res.redirect('/')
+            return
         }
+
         torrentClient.add(magnet_torrent, { path: __dirname + '/downloads/' + name }, function (torrent) {
-            var maxLength = 0
-            var correctFile = null
-            var torrentHash = torrent.infoHash
-            for (var i = torrent.files.length - 1; i >= 0; i--) {
-                file = torrent.files[i]
-                if (file.length > maxLength) {
-                    correctFile = file
-                    maxLength = file.length
-                }
-            }
-
-            stream = correctFile.createReadStream()
-
-            new Transcoder(stream)
-                .videoCodec('libx264')
-                .videoBitrate(req.body.quality * 1000)
-                .audioCodec('aac')
-                .audioBitrate(128 * 1000)
-                .format('mp4')
-                .on('finish', function() {
-                    next();
-                })
-                .on('error', function() {
-                    res.send("Error processing video")
-                })
-                .on('metadata', function(info) {
-                    size = info.input.duration * req.body.quality / 8
-                    res.writeHead(200, {
-                        'Content-Disposition': 'inline; filename="' + torrentHash + '.mp4"',
-                        'Content-Type': 'video/*',
-                        'Content-Range': 'bytes 0-' + size-1 + '/' + size,
-                        'Accept-Ranges': 'bytes',
-                        'Content-Length': size
-                    })
-                })
-                .stream().pipe(res)
+            res.redirect('/v/q' + req.body.quality + '/' + torrent.infoHash + '.mp4')
         })
 
         torrentClient.on('error', function (error) {
-            error_string = error.toString()
-            if (error_string.indexOf("duplicate")!=-1) {
-                var torrentHash = error_string.split(" ").pop()
-                torrent = torrentClient.get(torrentHash)
-
-                var maxLength = 0
-                var correctFile = null
-                for (var i = torrent.files.length - 1; i >= 0; i--) {
-                    file = torrent.files[i]
-                    if (file.length > maxLength) {
-                        correctFile = file
-                        maxLength = file.length
-                    }
-                }
-
-                stream = correctFile.createReadStream()
-
-                new Transcoder(stream)
-                    .videoCodec('libx264')
-                    .videoBitrate(req.body.quality * 1000)
-                    .audioCodec('aac')
-                    .audioBitrate(128 * 1000)
-                    .format('mp4')
-                    .on('finish', function() {
-                        next();
-                    })
-                    .on('error', function() {
-                        res.send("Error processing video")
-                    })
-                    .on('metadata', function(info) {
-                        size = info.input.duration * req.body.quality / 8
-                        res.writeHead(200, {
-                            'Content-Disposition': 'inline; filename="' + torrentHash + '.mp4"',
-                            'Content-Type': 'video/*',
-                            'Content-Range': 'bytes 0-' + size-1 + '/' + size,
-                            'Accept-Ranges': 'bytes',
-                            'Content-Length': size
-                        })
-                    })
-                    .stream().pipe(res)
-            }
+            res.redirect('/v/q' + req.body.quality + '/' + error.toString().split(" ").pop() + '.mp4')
         })
     } catch (e) {
-        console.log(e)
         next(e)
     }
 })
