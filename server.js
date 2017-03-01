59cf76d3765bea8af3f5247a5cd137fe05d3e660
victor141516 <yomellamovictor@gmail.com>
2017-03-01 01:38:14 +0100

    refactor and improvements

--- server.js
+++ server.js
@@ -7,97 +7,211 @@ var express = require('express')
   , multer  = require('multer')
   , storage = multer.memoryStorage()
   , upload = multer({ storage: storage })
-  , torrentClient = new (require('webtorrent'))
+  , WebTorrent = new (require('webtorrent'))
   , Transcoder = require('stream-transcoder')
+  , Promise = require('promise')
+  , debug = false
+  , bitrateRange = {'min': 400, 'max': 2000, 'default': 2000}
+  , servingPort = 3000
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 app.use(bodyParser.urlencoded({
     extended: true
-}));
-app.use(bodyParser.json());
+}))
+app.use(bodyParser.json())
 
-app.get('/v/q:quality/:hash_url', function (req, res, next) {
-    try {
-        hash = req.params.hash_url.split(".mp4")[0]
-        var torrent = torrentClient.get(hash)
-        var maxLength = 0
-        var correctFile = null
-        var audioBitRate = 128
 
-        console.log("View url (hash: " + hash + ")")
+function getBiggestFile(files) {
+    correct = files[0]
+    for (var i = files.length - 1; i >= 0; i--) {
+        file = files[i]
+        if (file.length > correct.length) {
+            correct = file
+        }
+    }
+    return correct
+}
 
-        for (var i = torrent.files.length - 1; i >= 0; i--) {
-            file = torrent.files[i]
-            if (file.length > maxLength) {
-                correctFile = file
-                maxLength = file.length
+function getTorrentFromRequest(req) {
+    if (undefined !== req.body.magnet && req.body.magnet !== '') {
+        if (debug) console.log('Debug: ' + 'Magnet taken')
+        return {
+            'torrent': req.body.magnet,
+            'name': req.body.magnet
+        }
+    } else if (undefined !== req.file) {
+        if (debug) console.log('Debug: ' + 'Torrent file taken')
+        return {
+            'torrent': req.file.buffer,
+            'name': req.file.originalname
+        }
+    } else {
+        if (debug) console.log('Debug: ' + 'Torrent/magen adquisition error')
+        return null
+    }
+}
+
+function getStream(file, videoBitrate, audioBitrate, start_end) {
+    return new Promise(
+        function (resolve, reject) {
+            if (videoBitrate == 'ORG') videoBitrate = bitrateRange.max
+            if (isNaN(parseInt(videoBitrate))) {
+                reject()
             }
-        }
-        console.log("File: " + (correctFile ? correctFile.name : correctFile))
+            if (videoBitrate > bitrateRange.max) videoBitrate = bitrateRange.max
+            if (videoBitrate < 1) videoBitrate = bitrateRange.min
+            quality = parseInt(videoBitrate)
 
-        var quality = req.params.quality
+            file_stream = file.createReadStream(start_end)
+            if (quality == bitrateRange.max) {
+                if (debug) console.log('Debug: ' + 'Size: ' + file.length.toString())
+                resolve({
+                    'httpCode': start_end ? 206 : 200,
+                    'size': file.length,
+                    'stream': file_stream
+                })
+            } else {
+                transcoder = new Transcoder(file_stream)
+                    .videoCodec('libx264')
+                    .videoBitrate(quality * 1000)
+                    .audioCodec('aac')
+                    .audioBitrate(audioBitrate * 1000)
+                    .channels(2)
+                    .format('mp4')
+                    // .custom('movflags', 'faststart')
+                transcoded_stream = transcoder.stream()
 
-        if (quality == 'ORG') {
-            console.log("Quality: ORIGINAL")
-            console.log("Size: " + maxLength.toString())
-            res.writeHead(200, {
-                'Content-Type': 'video/*',
-                'Content-Length': maxLength
+                transcoder.on('error', error => {
+                    if (debug) console.log('Debug: ' + error)
+                    reject(error)
+                })
+                transcoder.on('metadata', info => {
+                    if (debug) console.log('Debug: ' + info.input.duration)
+                    resolve({
+                        'httpCode': start_end ? 206 : 200,
+                        'size': info.input.duration * (audioBitrate + quality / 1000),
+                        'stream': transcoded_stream
+                    })
+                })
+            }
+        })
+}
+
+function addTorrent(input_torrent, name) {
+    return new Promise(
+        function (resolve, reject) {
+            WebTorrent.add(input_torrent, { path: __dirname + '/downloads/' + md5(name) }, torrent => {
+                torrent.on('error', error => {
+                    if (debug) console.log('Debug: ' + 'Torrent error')
+                    if (debug) console.log('Debug: ' + error)
+                    reject()
+                })
+                if (debug) console.log('Debug: ' + 'Torrent added')
+                resolve(torrent.infoHash)
             })
-            correctFile.createReadStream().pipe(res)
-        } else if (parseInt(quality)) {
-            console.log("Quality: " + quality)
 
-            // new Transcoder(correctFile.createReadStream())
-            //     .custom()
-            //     .on('error', function(error) {console.log(error)})
-            //     .on('metadata', function(info) {
-            //         var size = info.input.duration * (parseInt(quality) + audioBitRate) / 8
-            //         console.log("Calculated transcoded size: " + size.toString())
-            //         // if (!req.headers.range) {
-            //         //     res.writeHead(200, {
-            //         //         'Content-Type': 'video/*',
-            //         //         // 'Content-Range': 'bytes 0-' + (size-1).toString() + '/' + size.toString(),
-            //         //         // 'Accept-Ranges': 'bytes',
-            //         //         'Content-Length': size.toString()
-            //         //     })
-            //         // } else  {
-            //         //     start = parseInt(req.headers.range.replace(/bytes=/, "").split("-")[0], 10)
-            //             res.writeHead(200, {
-            //                 'Content-Type': 'video/*',
-            //                 //'Content-Range': 'bytes ' + start.toString() + '-' + (size-1).toString() + '/' + size.toString(),
-            //                 //'Accept-Ranges': 'bytes',
-            //                 // 'Content-Length': ((size - start) + 1).toString()
-            //                 'Content-Length': size.toString()
-            //             })
-
-                        new Transcoder(correctFile.createReadStream())
-                            .videoCodec('libx264')
-                            .videoBitrate(parseInt(quality) * 1000)
-                            .audioCodec('aac')
-                            .audioBitrate(audioBitRate * 1000)
-                            .format('mp4')
-                            .on('finish', function() {
-                                next();
-                            })
-                            .on('error', function(error) {console.log(error)})
-                            .stream().pipe(res)
-                    // }
-                // }).exec()
-        } else {
-            res.redirect('/')
+            WebTorrent.on('error', error => {
+                if (error.toString().indexOf('duplicate') < 0) {
+                    if (debug) console.log('Debug: ' + 'Torrent client error')
+                    if (debug) console.log('Debug: ' + error)
+                    reject()
+                } else {
+                    if (debug) console.log('Debug: ' + 'Duplicate torrent')
+                    resolve(error.toString().split(' ').pop())
+                }
+            })
         }
+    )
+}
+
+app.get('/v/q:quality/:hash_url', (req, res, next) => {
+    try {
+        const compiledFunction = pug.compileFile(__dirname + '/source/views/player.pug')
+        var html = compiledFunction({'video_src': '/s/q' + req.params.quality + '/' + req.params.hash_url})
+        res.send(html)
     } catch (e) {
-        console.log(e)
+        if (debug) console.log('Debug: ' + e)
+        res.redirect('/d/q' + req.params.quality + '/' + req.params.hash_url)
+        next(e)
+    }
+})
+
+app.get('/d/q:quality/:hash_url', (req, res, next) => {
+    try {
+        var torrent = WebTorrent.get(req.params.hash_url.split('.mp4')[0])
+          , correctFile = getBiggestFile(torrent.files)
+          , quality = req.params.quality
+
+        if (debug) console.log('Debug: ' + 'View url (hash: ' + (req.params.hash_url.split('.mp4')[0]) + ')')
+        if (debug) console.log('Debug: ' + 'File: ' + (correctFile ? correctFile.name : correctFile))
+        if (debug) console.log('Debug: ' + 'Quality: ' + quality)
+
+        getStream(correctFile, quality, 128)
+            .then(result => {
+                res.writeHead(result.httpCode, {
+                    'Content-Type': 'video/*',
+                    'Content-Length': result.size
+                })
+                result.stream.pipe(res)
+            })
+            .catch(error => {
+                console.log(error)
+                res.redirect('/')
+            })
+    } catch (e) {
+        if (debug) console.log('Debug: ' + e)
         res.redirect('/')
         next(e)
     }
 })
 
-app.get('/', function (req, res, next) {
+app.get('/s/q:quality/:hash_url', (req, res, next) => {
     try {
-        var html = pug.renderFile(__dirname + '/source/views/upload.pug')
+        var torrent = WebTorrent.get(req.params.hash_url.split('.mp4')[0])
+          , correctFile = getBiggestFile(torrent.files)
+          , quality = req.params.quality
+
+        if (debug) console.log('Debug: ' + 'View url (hash: ' + (req.params.hash_url.split('.mp4')[0]) + ')')
+        if (debug) console.log('Debug: ' + 'File: ' + (correctFile ? correctFile.name : correctFile))
+        if (debug) console.log('Debug: ' + 'Quality: ' + quality)
+
+        getStream(correctFile, quality, 128)
+            .then(result => {
+                var range = req.headers.range;
+                var positions = range.replace(/bytes=/, "").split("-");
+                var start = parseInt(positions[0], 10);
+                var total = result.size;
+                var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+                var chunksize = (end - start) + 1;
+
+                res.writeHead(result.httpCode, {
+                    "Content-Range": "bytes " + start + "-" + end + "/" + total,
+                    "Accept-Ranges": "bytes",
+                    "Content-Length": chunksize,
+                    "Content-Type": "video/mp4"
+                });
+                result.stream.pipe(res)
+            })
+            .catch(error => {
+                console.log(error)
+                res.redirect('/')
+            })
+    } catch (e) {
+        if (debug) console.log('Debug: ' + e)
+        res.redirect('/')
+        next(e)
+    }
+})
+
+app.get('/', (req, res, next) => {
+    try {
+        const compiledFunction = pug.compileFile(__dirname + '/source/views/upload.pug')
+        var html = compiledFunction({
+            'min_bitrate': bitrateRange.min,
+            'max_bitrate': bitrateRange.max,
+            'def_bitrate': bitrateRange.default
+        })
         res.send(html)
     } catch (e) {
         res.redirect('/')
@@ -105,39 +219,29 @@ app.get('/', function (req, res, next) {
     }
 })
 
-app.post('/upload', upload.single('torrent'), function (req, res, next) {
+app.post('/upload', upload.single('torrent'), (req, res, next) => {
     try {
-        if (undefined !== req.body.magnet && req.body.magnet !== '') {
-            magnet_torrent = req.body.magnet
-            name = magnet_torrent
-            console.log("Magnet taken")
-        } else if (undefined !== req.file) {
-            magnet_torrent = req.file.buffer
-            name = req.file.originalname
-            console.log("Torrent file taken")
-        } else {
-            console.log("Torrent/magen adquisition error")
-            res.redirect('/')
-            return
-        }
-
-        torrentClient.add(magnet_torrent, { path: __dirname + '/downloads/' + md5(name) }, function (torrent) {
-            console.log("Torrent added")
-            console.log("Download path: " + torrent.path)
-            res.redirect('/v/q' + req.body.quality + '/' + torrent.infoHash + '.mp4')
-        })
-
-        torrentClient.on('error', function (error) {
-            console.log("Torrent client error")
-            console.log(error)
-            res.redirect('/')
-        })
+        torrent_info = getTorrentFromRequest(req)
+        addTorrent(torrent_info.torrent, torrent_info.name)
+            .then(hash => {
+                addr = '/'
+                    + ((req.body.view === undefined) ? 'd' : 'v')
+                    + '/q' + req.body.quality
+                    + '/' + hash + '.mp4'
+                if (debug) console.log('Debug: ' + 'Redirect: ' + addr)
+                res.redirect(addr)
+            })
+            .catch(error => {
+                if (debug) console.log('Debug: ' + error.toString())
+                if (debug) console.log('Debug: ' + 'Redirect (on error): /')
+                res.redirect('/')
+            })
     } catch (e) {
         res.redirect('/')
         next(e)
     }
 })
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || servingPort, () => {
+    if (debug) console.log('Debug: ' + 'Serving')
 })
